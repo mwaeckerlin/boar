7a4af0b8cd638358b09c26637273f251625df323
Marc Wäckerlin <marc@waeckerlin.org>
2016-04-01 14:25:02 +0200

    more information on start

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@
 /config-ldap.sh
 /etc/init.d/nscd start
 if ! test -d /home/${BOAR_USER}; then
+    echo "setup /boar ..."
     useradd -ms /bin/bash -d /home/$BOAR_USER -g $BOAR_GROUP $BOAR_USER
     chown -R ${BOAR_USER}.${BOAR_GROUP} /boar
     chmod -R g+w /boar
 fi
-/usr/sbin/sshd -D
+
+echo "ready, starting ssh daemon..."
+/usr/sbin/sshd $SSHOPTIONS -D
